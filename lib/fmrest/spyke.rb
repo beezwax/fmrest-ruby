50ea54ea0d1db4615ffc2cc460b7d3be81fc284b
Pedro Carbajal <pedro_c@beezwax.net>
2019-02-12 20:39:11 -0300

    Autoload Spyke

--- lib/fmrest/spyke.rb
+++ lib/fmrest/spyke.rb
@@ -1,3 +1,10 @@
+begin
+  require "spyke"
+rescue LoadError => e
+  e.message << " (Did you include Spyke in your Gemfile?)"
+  raise e
+end
+
 require "fmrest/spyke/json_parser"
 require "fmrest/spyke/model"
 require "fmrest/spyke/base"
