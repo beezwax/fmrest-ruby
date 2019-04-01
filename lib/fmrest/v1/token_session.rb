ca294039ad5af580389f13b23b0786974bb4e4b9
ArthurWeiden <ajpwvanderweiden@gmail.com>
2019-04-01 12:26:19 +0200

    ""

--- lib/fmrest/v1/token_session.rb
+++ lib/fmrest/v1/token_session.rb
@@ -70,7 +70,7 @@ module FmRest
             # Strip the host part to just the hostname (i.e. no scheme or port)
             host = @options.fetch(:host)
             host = URI(host).hostname if host.match?(/\Ahttps?:\/\//)
-            multi_tenant = @options.fetch(:multi_tenancy).nil? ? '' : ":#{@options.fetch(:multi_tenancy)}"
+            multi_tenant = @options[:multi_tenancy].nil? ? '' : ":#{@options[:multi_tenancy]}"
             puts "THE DATABASE IS #{host}:#{@options.fetch(:database)}#{multi_tenant}"
             "#{host}:#{@options.fetch(:database)}#{multi_tenant}"
           end
