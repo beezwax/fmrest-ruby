d2a818482402c2b8061416491e19ad43add9201f
Pedro Carbajal <pedro_c@beezwax.net>
2019-10-02 19:46:38 +0900

    Use a copy of ERB::Util.url_encode to remove dependency on ERB

--- lib/fmrest/v1/paths.rb
+++ lib/fmrest/v1/paths.rb
@@ -10,24 +10,33 @@ module FmRest
       end
 
       def record_path(layout, id = nil)
-        url = "layouts/#{ERB::Util.url_encode(layout.to_s)}/records"
+        url = "layouts/#{url_encode(layout)}/records"
         url += "/#{id}" if id
         url
       end
 
       def container_field_path(layout, id, field_name, field_repetition = 1)
         url = record_path(layout, id)
-        url += "/containers/#{ERB::Util.url_encode(field_name.to_s)}"
+        url += "/containers/#{url_encode(field_name)}"
         url += "/#{field_repetition}" if field_repetition
         url
       end
 
       def find_path(layout)
-        "layouts/#{ERB::Util.url_encode(layout.to_s)}/_find"
+        "layouts/#{url_encode(layout)}/_find"
       end
 
       def globals_path
         "globals"
+      end
+
+      private
+
+      # Borrowed from ERB::Util
+      def url_encode(s)
+        s.to_s.b.gsub(/[^a-zA-Z0-9_\-.]/n) { |m|
+          sprintf("%%%02X", m.unpack("C")[0])
+        }
       end
     end
   end
