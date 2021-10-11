311ca653e266e1e434b78447600466329ace1e9c
Pedro Carbajal <pedro_c@beezwax.net>
2021-10-11 13:11:50 +0900

    Fix syntax error in TokenStore::Null

--- lib/fmrest/token_store/null.rb
+++ lib/fmrest/token_store/null.rb
@@ -1,20 +1,11 @@
 # frozen_string_literal: true
 
-require "singleton"
-
 module FmRest
   module TokenStore
-    module Null < Base
-      include Singleton
-
-      def delete(key)
-      end
-
-      def load(key)
-      end
-
-      def store(key, value)
-      end
+    class Null < Base
+      def delete(key); end
+      def load(key); end
+      def store(key, value); end
     end
   end
 end
