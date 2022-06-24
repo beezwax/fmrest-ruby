c6053b7c75ba6ae11184b9b329b3d02878b0701f
Pedro Carbajal <pedro_c@beezwax.net>
2022-06-24 19:50:40 +0900

    Map DAPI exceptions to HTTP responses in Rails

--- lib/fmrest/railtie.rb
+++ lib/fmrest/railtie.rb
@@ -5,6 +5,25 @@ require "fmrest"
 module Rails
   module FmRest
     class Railtie < Rails::Railtie
+      # Mapping of rescued exceptions to HTTP responses
+      #
+      # @example
+      #   railtie.rescue_responses
+      #
+      # @ return [Hash] rescued responses
+      def self.rescue_responses
+        {
+          "FmRest::APIError::RecordMissingError"     => :not_found,
+          "FmRest::APIError::NoMatchingRecordsError" => :not_found,
+          "FmRest::APIError::ValidationError"        => :unprocessable_entity,
+          "FmRest::Spyke::ValidationError"           => :unprocessable_entity,
+        }
+      end
+
+      if config.action_dispatch.rescue_responses
+        config.action_dispatch.rescue_responses.merge!(rescue_responses)
+      end
+
       initializer "fmrest.load_config" do
         ::FmRest.default_connection_settings = Rails.application.config_for("fmrest")
       end
